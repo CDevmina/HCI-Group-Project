0da340688ac3dca999cf391c9a1dc9f16401dbe0
Gagana Methmal (GM) <gaganam220@gmail.com>
2025-04-26 17:00:54 +0530

    Ui's
    Home Page, Products Page, Product Details Pages completed.

--- ikae/src/App.jsx
+++ ikae/src/App.jsx
@@ -12,12 +12,15 @@ import ErrorPage from "./pages/Error";
 import RoomDesigner from "./pages/Room";
 import RegisterPage from "./pages/Register";
 import RoomEditor from "./scenes/RoomEditor/RoomEditor";
+import HomePage from "./pages/Home";
+import ProductsPage from "./pages/Products";
+import ProductDetailsPage from "./pages/Details";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<RoomEditor/>} />
+        <Route path="/" element={<RoomEditor />} />
         <Route path="/shop" element={<Shop />} />
         <Route path="/editor" element={<Editor />} />
         <Route path="/signup" element={<Signup />} />
@@ -30,6 +33,9 @@ function App() {
         <Route path="/room" element={<RoomDesigner />} />
         <Route path="/profile" element={<UserProfileSettingsPage />} />
         <Route path="/help" element={<HelpDocumentationPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/details" element={<ProductDetailsPage />} />
       </Routes>
     </BrowserRouter>
   );
